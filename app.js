ba7fa76bbddbc1d0005978bd66cc6103e82935ac
fthgncl <gencal.fatih61@gmail.com>
2025-09-26 16:46:09 +0300

    .env dosyasını güncelleme notu eklendi (i18nConfig.js).

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const logError = require('./src/utils/logger');
 const loadEnv = require('./src/utils/loadEnv');
 const colors = require('ansi-colors');
 
+// TODO: .env dosyasını güncelle ( i18nConfig.js )
+
 (async () => {
     let t = null;
 
