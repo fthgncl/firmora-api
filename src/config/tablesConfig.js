6fef3a3e2f0746a9d4a7995f96cedfba129122c0
fthgncl <gencal.fatih61@gmail.com>
2025-09-28 13:52:43 +0300

    `tablesConfig: phone alanı NOT NULL olarak güncellendi`

--- src/config/tablesConfig.js
+++ src/config/tablesConfig.js
@@ -4,7 +4,7 @@ module.exports = {
         name: 'VARCHAR(20) NOT NULL CHECK (CHAR_LENGTH(name) >= 3)',
         surname: 'VARCHAR(20) NOT NULL CHECK (CHAR_LENGTH(surname) >= 3)',
         username: 'VARCHAR(15) NOT NULL UNIQUE CHECK (CHAR_LENGTH(username) >= 6)',
-        phone: 'VARCHAR(20) UNIQUE',
+        phone: 'VARCHAR(20) NOT NULL UNIQUE',
         password: 'VARCHAR(255)',
         created_at: 'DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP',
         permissions: 'VARCHAR(50) NOT NULL DEFAULT \'\' CHECK (permissions REGEXP \'^[a-zA-Z]*$\')',
