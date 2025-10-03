b9b4cb484c7a4be2681d7fbad103984818aac507
fthgncl <gencal.fatih61@gmail.com>
2025-10-03 10:44:30 +0300

    Şirket güncelleme işlevi için route'lar yorum satırına alındı.

--- src/express/routes/companies.js
+++ src/express/routes/companies.js
@@ -4,12 +4,13 @@ const router = express.Router();
 // Company route handlers
 const createCompanyRoute = require('./companies/createCompany');
 const listCompaniesRoute = require('./companies/listCompanies');
-const updateCompanyRoute = require('./companies/updateCompany');
+//const updateCompanyRoute = require('./companies/updateCompany');
 const deleteCompanyRoute = require('./companies/deleteCompany');
 
 // Company routes
 router.use('/', createCompanyRoute);
 router.use('/', listCompaniesRoute);
+//router.use('/', updateCompanyRoute);
 router.use('/', deleteCompanyRoute);
 
 module.exports = router;
