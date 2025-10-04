3480c1f4c72d30b05e47ad6da597bbc7d933cb38
fthgncl <gencal.fatih61@gmail.com>
2025-10-04 23:41:18 +0300

    İzin yapılandırmasından 'create_company' alanı silindi.

--- src/config/permissionsConfig.js
+++ src/config/permissionsConfig.js
@@ -5,12 +5,6 @@ const permissions = {
         "category": "system",
         "name_key": "permissions.sys_admin.name",
         "description_key": "permissions.sys_admin.description"
-    },
-    "create_company": {
-        "code": "b",
-        "category": "company",
-        "name_key": "permissions.create_company.name",
-        "description_key": "permissions.create_company.description"
     }
 }
 module.exports = permissions;
