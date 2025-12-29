b4e13bdb3d43137f3a45f5df1d3870fd8da26401
fthgncl <gencal.fatih61@gmail.com>
2025-12-29 03:10:42 +0300

    Add account update messages and integrate updateAccount function

--- src/database/accounts/index.js
+++ src/database/accounts/index.js
@@ -3,11 +3,13 @@ const getAccountsByUserId = require('./getAccountsByUserId');
 const getAccountById = require('./getAccountById');
 const addAccountBalance = require('./addAccountBalance');
 const deductAccountBalance = require('./deductAccountBalance');
+const updateAccount = require('./updateAccount');
 
 module.exports = {
     createAccount,
     getAccountsByUserId,
     addAccountBalance,
     deductAccountBalance,
-    getAccountById
+    getAccountById,
+    updateAccount
 };
